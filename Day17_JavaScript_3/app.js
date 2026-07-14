6db9c256af3976d9e1aafc27d55d5d5538d025cb
Raj Kumar <rajkumargupta00423@gmail.com>
2026-07-14 21:47:39 +0530

    Day17_JavaScript_3

---
+++ Day17_JavaScript_3/app.js
@@ -0,0 +1,24 @@
+
+// let name = "EduTechRaj";
+// let msg ="ILOVECODING";
+
+// msg.indexOf("LOVE");
+
+// let msg ="       ILOVECODING    ";
+// let newMsg = msg.trim();
+// console.log("After Trim:", newMsg);
+// newMsg =newMsg.toUpperCase();
+// console.log("After Upper Case: ",newMsg);
+
+
+// Slice
+
+// let msg =" hello";
+// console.log(msg.slice(2)); 
+
+let msg ="Hello!";
+console.log(msg.trim().toUpperCase());
+
+
+let met ="EdutechRaj";
+name.slice(4,7);
