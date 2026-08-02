f7ca74ee8180c25714d6198340f2db98d1b04137
Raj Kumar Gupta <rajkumargupta00423@gmail.com>
2026-08-02 22:32:37 +0530

    Day35_Node

---
+++ Day35_Node/Backend/script.js
@@ -0,0 +1,30 @@
+// let n=5;
+
+// for(let i=0; i<n;i++){
+//     console.log("hello,",i);
+// }
+
+// console.log("Bye!!");\\
+
+// let args =process.argv;
+
+// for(let i=0;i<args.length;i++){
+//     console.log("Helo & Welcome to",args[i]);
+// }
+// console.log(process.argv);
+
+// const math = require("./math");
+
+// console.log(math.sum(2,5));
+// console.log(math.mul(2,5));
+// console.log(math.g);
+// console.log(math.PI);
+
+// const info = require("./Fruits");
+// console.log(info);
+// console.log(info[0]);
+// console.log(info[2].name);
+
+
+import {sum,PI} from "./math.js";
+
